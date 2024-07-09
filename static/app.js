58dc2d1af0391f4621b0a1e64f2c6ecd8436f149
Israel-codes <onlineservice.spport2024@gmail.com>
2024-07-09 12:28:47 +0100

    changed website url

--- static/app.js
+++ static/app.js
@@ -31,7 +31,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-         fetch(' http://127.0.0.1:5000/predict', {
+         fetch(' https://medical-chatbot-pk8c.onrender.com/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
