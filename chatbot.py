0be67e7b2426405d8f9b42ea24d4051551f2f8ba
Israel-codes <onlineservice.spport2024@gmail.com>
2024-07-09 12:13:14 +0100

    changed ai response

--- chatbot.py
+++ chatbot.py
@@ -48,7 +48,7 @@ def get_response(msg):
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
     
-    return "I do not understand..."
+    return "To better understand your situation, can you tell me more about your symptom?"
 
 
 if __name__ == "__main__":
